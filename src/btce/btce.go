56a5674f6637e3c251813584ee12c645d846270f
travisstone <tstone@gmail.com>
2014-03-18 00:49:52 -0500

    Create btce.go

---
+++ src/btce/btce.go
@@ -0,0 +1,50 @@
+package btce
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+//	"strings"
+)
+
+
+func Balance() string {
+
+/*		type TickerJSON struct {
+			ticker struct {
+				high string
+				low string
+				avg string
+				vol string
+				vol_cur string
+				last string
+				buy string
+				sell string
+				updated string
+				server_time string
+				}
+			}
+*/
+
+		btce, err := http.Get("https://btc-e.com/api/2/ltc_btc/ticker")
+    	if err != nil {log.Fatal(err)}
+    	btcedata, err := ioutil.ReadAll(btce.Body)
+    	btce.Body.Close()
+    	if err != nil {log.Fatal(err)}
+		
+		var btceTickerJSON interface {}
+
+		btceTickerbloberr := json.Unmarshal(btcedata, &btceTickerJSON)
+		if btceTickerbloberr != nil {
+			fmt.Printf("BTCE Ticker JSON Error : %v\n", btceTickerbloberr)
+			}
+			
+		TickerJSON := btceTickerJSON.(map[string]interface{})
+		btceticker := TickerJSON["ticker"].(map[string]interface{})
+
+
+//		fmt.Printf("BTCE ask price: %v\n", btceticker["buy"])
+		return fmt.Sprint(btceticker["buy"])
+}
